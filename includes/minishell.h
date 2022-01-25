ed912702ef48c572bd70c8901c22436c06ea48a0
Thomas Nanchen <thomasnanchen@hotmail.com>
2022-01-25 15:40:33 +0100

    message

---
+++ includes/minishell.h
@@ -0,0 +1,15 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include "../libft/libft.h"
+
+/* terminal colors */
+# define RED "\033[0;31m"
+# define GRE "\033[0;32m"
+# define YEL "\033[0;33m"
+# define BLU "\033[0;34m"
+# define MAG "\033[0;35m"
+# define CYA "\033[0;36m"
+# define WHI "\033[0m"
+
+#endif
--- srcs/main.c
+++ srcs/main.c
@@ -1,4 +1,11 @@
-#include "../libft/libft.h"
+#include "../includes/minishell.h"
+
+void	print_prompt(void)
+{
+	ft_putstr_fd(CYA,1);
+	ft_putstr_fd("prompt$ ",1);
+	ft_putstr_fd(WHI,1);
+}
 
 int	main(int ac, char **av, char **env)
 {
@@ -10,7 +17,7 @@ int	main(int ac, char **av, char **env)
 	while (1)
 	{
 		// remplacer par print_prompt() :
-		ft_printf("our prompt $");
+		print_prompt();
 		if ((cmd = get_next_line(STDIN_FILENO)))
 		{
 			// fork
