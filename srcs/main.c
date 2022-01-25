e0b6f8f4e416810441c4223327acbf12f57ac86c
Thomas Nanchen <thomasnanchen@hotmail.com>
2022-01-25 15:29:08 +0100

    message

--- srcs/main.c
+++ srcs/main.c
@@ -5,5 +5,21 @@ int	main(int ac, char **av, char **env)
 	(void) ac;
 	(void) av;
 	(void) env;
-	ft_printf("Makefile test\n");
+	char *cmd = NULL;
+
+	while (1)
+	{
+		// remplacer par print_prompt() :
+		ft_printf("our prompt $");
+		if ((cmd = get_next_line(STDIN_FILENO)))
+		{
+			// fork
+			// execute cmd
+			// wait
+
+
+
+			ft_printf("%s", cmd);
+		}
+	}
 }
